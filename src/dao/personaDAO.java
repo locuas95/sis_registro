72e70680883302395133d395cb484c45d5c0eebf
cmalaspina1 <cmalaspina1@cmalaspina>
2016-08-16 23:00:48 -0500

    Parte-4	malaspina

--- src/dao/personaDAO.java
+++ src/dao/personaDAO.java
@@ -22,7 +22,7 @@ public class personaDAO implements Operaciones<personaDTO>{
     private static PreparedStatement ps;
     
     private final String sql_create="INSERT INTO bd_registrop.persona(idpersona,nombre,apellido,direccion,telefono,dni)VALUES (NULL ,?,?,?,?,?);";
-    private final String sql_update="";
+    private final String sql_update="UPDATE bd_registrop.persona SET nombre=?,apellido=?,direccion=?, telefono=?,dni=? WHERE persona.idpersona=?";
     private final String sql_delete="";
     private final String sql_read="";
     private final String sql_readAll="SELECT *FROM persona";
@@ -51,7 +51,23 @@ public class personaDAO implements Operaciones<personaDTO>{
 
     @Override
     public int update(personaDTO e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int op = 0;        
+        try {
+            cx = conexion.getConexion();
+            ps = cx.prepareStatement(sql_update);
+            ps.setString(1, e.getNombre());
+            ps.setString(2, e.getApellido());
+            ps.setString(3, e.getDireccion());
+            ps.setString(4, e.getTelefono());
+            ps.setString(5, e.getDni());
+            ps.setInt(6, e.getIdpersona());
+           op = ps.executeUpdate();                    
+        } catch (Exception x) {
+            JOptionPane.showMessageDialog(null, "updateproducto"+x);
+        } finally{
+           conexion.cerrar();
+        }
+        return op;
     }
 
     @Override
